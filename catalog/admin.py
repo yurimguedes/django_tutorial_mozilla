c48c05e1918a91a0d39b9a17727a3f85f3061c94
Yuri Guedes <yuri@webemlata.com.br>
2022-02-10 14:12:37 -0300

    Adicionando filtros

--- catalog/admin.py
+++ catalog/admin.py
@@ -22,4 +22,5 @@ class BookAdmin(admin.ModelAdmin):
 # Register the Admin classes for BookInstance using the decorator
 @admin.register(BookInstance)
 class BookInstanceAdmin(admin.ModelAdmin):
+    list_filter = ('status', 'due_back')
     pass
